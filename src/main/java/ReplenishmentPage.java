11e4487cc762704545a860a095d50be8324e7ffd
Marie Fedorchenko <manya19111@gmail.com>
2024-11-29 15:52:55 +0300

    Домашнее задание по уроку №15.

---
+++ src/main/java/ReplenishmentPage.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebDriver;
+
+public class ReplenishmentPage {
+
+    protected WebDriver driver;
+
+    public ReplenishmentPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
